671ee05963022ff0e094196a983bf0f8653abf90
Nicolas Simon <nimon@42paris.fr>
2024-03-19 09:35:12 +0000

    Rework for nimon.fr

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     outputStandalone: true,
   },
   env: {
-    base_url:'https://badge42.coday.fr',
-    github_repo:'https://github.com/Coday-meric/badge42'
+    base_url:'https://badge.nimon.fr',
+    github_repo:'https://github.com/Nimon77/badge42'
   },
   output: 'standalone',
 };
